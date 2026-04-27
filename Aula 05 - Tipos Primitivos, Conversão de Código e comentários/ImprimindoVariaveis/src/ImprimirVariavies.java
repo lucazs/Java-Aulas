5a0fca3ca62ab37b7c17d5805c7084d695e16459
lucazs <38823887+lucazs@users.noreply.github.com>
2026-04-27 23:26:55 +0000

    Aula 05

---
+++ Aula 05 - Tipos Primitivos, Conversão de Código e comentários/ImprimindoVariaveis/src/ImprimirVariavies.java
@@ -0,0 +1,15 @@
+public class ImprimirVariavies 
+{
+    public static void main(String[] args) 
+    {
+        // Cometários simples é só em uma única linha.
+        /*
+            Aqui um coméntario de mais linhas
+        */
+       /***
+        * @param args parametro de entrada do método man
+        * Coméntario oficial do java
+        */
+        System.out.println("Dados impressos");    
+    }
+}
---
+++ Aula 05 - Tipos Primitivos, Conversão de Código e comentários/Teoria.md
@@ -0,0 +1,5 @@
+# Tipos Primitivos, Conversão de Códigos e Comentários
+
+## Nomes de Classes
+
+Começa com letra maiusculas e a proxima palavra tbm começa "CamelCase"
