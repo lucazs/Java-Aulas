beaf3fcdeba0b34e511373b0e228fc43c719c946
lucazs <38823887+lucazs@users.noreply.github.com>
2026-04-28 00:06:42 +0000

    Aula 06

---
+++ Aula 06 Continuacao Tipos Primitivos, exercício e operadores/ImprimirVariaveis/src/App.java
@@ -0,0 +1,29 @@
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        int idade = 10;
+        double salarioDouble = 3000;
+        float salarioFloat = 3000;
+        byte idadeByte = 12;
+        short idadeShort = 32767;
+        boolean Verdadeiro = true;
+        boolean Falso = false;
+        long numeroGrande = 1000L;
+        char caractere = 'A';
+        String nome = "Lucas";
+        System.out.println("===============================");
+        System.out.println(idade);
+        System.out.println(idadeByte);
+        System.out.println(salarioDouble);
+        System.out.println(salarioFloat);
+        System.out.println(idadeShort);
+        System.out.println(Verdadeiro);
+        System.out.println(Falso);
+        System.out.println(numeroGrande);
+        System.out.println(caractere);
+        System.out.println(nome);
+        System.out.println("===============================");
+    }
+}
---
+++ Aula 06 Continuacao Tipos Primitivos, exercício e operadores/ImprimirVariaveis/src/Operadores.java
@@ -0,0 +1,71 @@
+public class Operadores 
+{
+    public static void main(String[] args) 
+    {
+        /***
+         * Operadores
+         * 1. Operadores Aritméticos
+         * 
+         * Usados para cálculos matemáticos básicos
+         * 
+         * +  : Soma ou Concatenação
+         * -  : Subtração
+         * *  : Multiplicação
+         * /  : Divisão
+         * %  : Resto da divisão
+         * 
+         * 2. Operadores Unários
+         * Operam em apenas um único operando. 
+         * 
+         * ++ : Incremento (aumenta o valor em 1).
+         * -- : Decremento (diminui o valor em 1).
+         * +  : Unário positivo (indica valor positivo).
+         * -  : Unário negativo (inverte o sinal do operando).
+         * !  : Negação lógica (inverte o valor booleano
+         * 
+         * 3. Operadores Relacionais (Comparação)
+         * 
+         * Comparam dois valores e retornam um resultado booleano (true ou false). 
+         * 
+         * == : Igual a.
+         * != : Diferente de.
+         * >  : Maior que.
+         * <  : Menor que.
+         * >= : Maior ou igual a.
+         * <= : Menor ou igual a. 
+         * 
+         * 4. Operadores Lógicos
+         * 
+         * Utilizados para combinar expressões booleanas. 
+         * 
+         * && : E lógico (AND) — retorna verdadeiro se ambos forem verdadeiros.
+         * || : OU lógico (OR) — retorna verdadeiro se pelo menos um for verdadeiro. 
+         * 
+         * 5. Operadores de Atribuição
+         * 
+         * Atribuem valores a variáveis. 
+         * 
+         * = : Atribuição simples.
+         * +=, -=, *=, /=, %= : Atribuições compostas (ex: x += 5 é o mesmo que x = x + 5).
+         * 
+         * 6. Operadores de Bits (Bitwise)
+         * 
+         * Realizam operações diretamente nos bits dos dados. 
+         * 
+         * & : AND bit a bit.
+         * | : OR bit a bit.
+         * ^ : XOR (OU exclusivo) bit a bit.
+         * ~ : Complemento bit a bit.
+         * << : Deslocamento à esquerda.
+         * >> : Deslocamento à direita.
+         * >>> : Deslocamento à direita preenchendo com zeros. 
+         * 
+         * 7. Outros Operadores Importantes
+         * 
+         * ?: : Operador Ternário — uma forma compacta de if-else.
+         * instanceof :  : Verifica se um objeto é uma instância de uma classe específica.
+         * (tipo) : Cast — converte um valor de um tipo para outro.
+         * 
+         *  */    
+    }
+}
